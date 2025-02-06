a5eec05d754a8a2d42c47b7e6cc4aa2705caa0e1
Danial Dabirian <danial@upera.tv>
2025-02-06 19:02:59 +0330

    feat: enable validation

--- src/main.ts
+++ src/main.ts
@@ -2,9 +2,11 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import ConfigService from './common/config/config.service';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe());
 
   const config = new DocumentBuilder()
     .setTitle('Job Offers')
