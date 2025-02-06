82dc2b70fc5587ffd0a8cacb254f7f536770638b
Danial Dabirian <danial@upera.tv>
2025-02-06 18:40:24 +0330

    feat: list port and repository

--- src/job-offers/infrastructure/database/postgres/job-offer-postgres.repository.ts
+++ src/job-offers/infrastructure/database/postgres/job-offer-postgres.repository.ts
@@ -3,9 +3,19 @@ import CreateJobOfferPort from '../../../domain/ports/outbounds/create-job-offer
 import { PrismaService } from '../../../../common/prisma/prisma.service';
 import JobOffer from '../../../domain/entities/job-offer.entity';
 import { JobOfferMapper } from '../../../job-offer.mapper';
+import {
+  IListJobOffersCommand,
+  IListJobOffersResult,
+} from '../../../domain/models/list-job-offers.command.interface';
+import ListJobOffersPort from '../../../domain/ports/outbounds/list-job-offers.port';
+import { IJobOfferSchema } from './job-offer.schema';
+import { Prisma } from '../../../../../prisma/clients/prisma.client';
+import JobOfferWhereInput = Prisma.JobOfferWhereInput;
 
 @Injectable()
-export class JobOfferPostgresRepository implements CreateJobOfferPort {
+export class JobOfferPostgresRepository
+  implements CreateJobOfferPort, ListJobOffersPort
+{
   constructor(
     private readonly prisma: PrismaService,
     private readonly jobOfferMapper: JobOfferMapper,
@@ -17,5 +27,54 @@ export class JobOfferPostgresRepository implements CreateJobOfferPort {
         data: this.jobOfferMapper.toPersistence(jobOffer),
       }),
     );
+  }
+
+  async list(filters: IListJobOffersCommand): Promise<IListJobOffersResult> {
+    const {
+      limit = 10,
+      offset = 0,
+      salaryMax,
+      salaryMin,
+      title,
+      location,
+    } = filters;
+    const conditions = {
+      ...(salaryMin && { salaryMin: { gte: +salaryMin } }),
+      ...(salaryMax && { salaryMax: { lte: +salaryMax } }),
+      ...(title && { title: { contains: title } }),
+      ...(location && { location: { contains: location } }),
+    };
+
+    const { jobOffers, total } = await this.listAndCount(
+      conditions,
+      limit,
+      offset,
+    );
+    const nextPageToken = +offset + +limit;
+
+    return {
+      jobOffers: jobOffers.map((jo) => this.jobOfferMapper.toDomain(jo)),
+      nextPageToken: `${nextPageToken < total ? nextPageToken : ''}`,
+      total,
+    };
+  }
+
+  private async listAndCount(
+    conditions: JobOfferWhereInput,
+    limit: number,
+    offset: number,
+  ): Promise<{ jobOffers: IJobOfferSchema[]; total: number }> {
+    const [jobOffers, total] = await Promise.all([
+      this.prisma.jobOffer.findMany({
+        where: conditions,
+        take: +limit,
+        skip: +offset,
+      }),
+      this.prisma.jobOffer.count({
+        where: conditions,
+      }),
+    ]);
+
+    return { jobOffers, total };
   }
 }
