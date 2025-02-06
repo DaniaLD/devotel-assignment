cfec6fecee80a424c5fb91a019d9dc014cb2bb3e
Danial Dabirian <danial@upera.tv>
2025-02-06 15:48:12 +0330

    feat: job offer port and repository

---
+++ src/job-offers/infrastructure/database/postgres/job-offer-postgres.repository.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@nestjs/common';
+import CreateJobOfferPort from '../../../domain/ports/outbounds/create-job-offer.port';
+import { PrismaService } from '../../../../common/prisma/prisma.service';
+import JobOffer from '../../../domain/entities/job-offer.entity';
+import { JobOfferMapper } from '../../../job-offer.mapper';
+
+@Injectable()
+export class JobOfferPostgresRepository implements CreateJobOfferPort {
+  constructor(
+    private readonly prisma: PrismaService,
+    private readonly jobOfferMapper: JobOfferMapper,
+  ) {}
+
+  async create(jobOffer: JobOffer): Promise<JobOffer> {
+    return this.jobOfferMapper.toDomain(
+      await this.prisma.jobOffer.create(
+        this.jobOfferMapper.toPersistence(jobOffer),
+      ),
+    );
+  }
+}
